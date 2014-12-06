ec5bf5abce9ad49e2bf7c98f4a52cb38ac9b0de6
bogdanelu83 <bogdanelu83@yahoo.com>
2014-12-07 00:08:55 +0200

    1st
    1st

---
+++ convertor/src/com/example/convertor/ActivityDespre.java
@@ -0,0 +1,16 @@
+package com.example.convertor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+@SuppressWarnings("unused")
+public class ActivityDespre extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_despre);
+	}
+}
