7cd2aa683107ccba76f0370bec173411487eafa4
bogdanelu83 <bogdanelu83@yahoo.com>
2014-12-07 19:23:20 +0200

    10th
    10th

--- ip2tara/gen/com/proiect/ip2tara/R.java
+++ ip2tara/gen/com/proiect/ip2tara/R.java
@@ -18,27 +18,27 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int ip_img=0x7f020001;
     }
     public static final class id {
-        public static final int action_despre=0x7f08000a;
-        public static final int action_setari=0x7f08000b;
-        public static final int action_settings=0x7f080009;
-        public static final int app_name=0x7f08000c;
-        public static final int autori=0x7f080002;
-        public static final int autori_nume=0x7f080003;
-        public static final int buton_tara=0x7f080001;
-        public static final int contact=0x7f080004;
-        public static final int contact_text=0x7f080005;
-        public static final int imageView1=0x7f080008;
-        public static final int intro=0x7f080007;
-        public static final int ip_address=0x7f080000;
-        public static final int sendEmail=0x7f080006;
+        public static final int action_despre=0x7f09000a;
+        public static final int action_setari=0x7f09000b;
+        public static final int action_settings=0x7f090009;
+        public static final int app_name=0x7f09000c;
+        public static final int autori=0x7f090002;
+        public static final int autori_nume=0x7f090003;
+        public static final int buton_tara=0x7f090001;
+        public static final int contact=0x7f090004;
+        public static final int contact_text=0x7f090005;
+        public static final int imageView1=0x7f090008;
+        public static final int intro=0x7f090007;
+        public static final int ip_address=0x7f090000;
+        public static final int sendEmail=0x7f090006;
     }
     public static final class layout {
         public static final int activity_cauta=0x7f030000;
@@ -49,39 +49,39 @@ public final class R {
         public static final int activity_tara=0x7f030005;
     }
     public static final class menu {
-        public static final int acces_bd=0x7f070000;
-        public static final int cauta=0x7f070001;
-        public static final int db=0x7f070002;
-        public static final int despre=0x7f070003;
-        public static final int istoric=0x7f070004;
-        public static final int main=0x7f070005;
-        public static final int setari=0x7f070006;
-        public static final int tara=0x7f070007;
+        public static final int acces_bd=0x7f080000;
+        public static final int cauta=0x7f080001;
+        public static final int db=0x7f080002;
+        public static final int despre=0x7f080003;
+        public static final int istoric=0x7f080004;
+        public static final int main=0x7f080005;
+        public static final int setari=0x7f080006;
+        public static final int tara=0x7f080007;
     }
     public static final class string {
-        public static final int action_despre=0x7f050006;
-        public static final int action_setari=0x7f050007;
-        public static final int action_settings=0x7f050005;
-        public static final int app_name=0x7f050000;
-        public static final int autori=0x7f05000e;
-        public static final int autori_nume=0x7f05000f;
-        public static final int buton_intra=0x7f050003;
-        public static final int buton_tara=0x7f050014;
-        public static final int compune_email=0x7f050012;
-        public static final int contact=0x7f050010;
-        public static final int contact_text=0x7f050011;
-        public static final int hello_world=0x7f050009;
-        public static final int image_desc=0x7f050004;
-        public static final int intro=0x7f050002;
-        public static final int ip_text=0x7f050013;
-        public static final int title_activity_acces_bd=0x7f050016;
-        public static final int title_activity_cauta=0x7f05000b;
-        public static final int title_activity_db=0x7f050015;
-        public static final int title_activity_despre=0x7f05000a;
-        public static final int title_activity_istoric=0x7f05000c;
-        public static final int title_activity_setari=0x7f050008;
-        public static final int title_activity_tara=0x7f05000d;
-        public static final int welcome=0x7f050001;
+        public static final int action_despre=0x7f060006;
+        public static final int action_setari=0x7f060007;
+        public static final int action_settings=0x7f060005;
+        public static final int app_name=0x7f060000;
+        public static final int autori=0x7f06000e;
+        public static final int autori_nume=0x7f06000f;
+        public static final int buton_intra=0x7f060003;
+        public static final int buton_tara=0x7f060014;
+        public static final int compune_email=0x7f060012;
+        public static final int contact=0x7f060010;
+        public static final int contact_text=0x7f060011;
+        public static final int hello_world=0x7f060009;
+        public static final int image_desc=0x7f060004;
+        public static final int intro=0x7f060002;
+        public static final int ip_text=0x7f060013;
+        public static final int title_activity_acces_bd=0x7f060016;
+        public static final int title_activity_cauta=0x7f06000b;
+        public static final int title_activity_db=0x7f060015;
+        public static final int title_activity_despre=0x7f06000a;
+        public static final int title_activity_istoric=0x7f06000c;
+        public static final int title_activity_setari=0x7f060008;
+        public static final int title_activity_tara=0x7f06000d;
+        public static final int welcome=0x7f060001;
     }
     public static final class style {
         /** 
@@ -105,10 +105,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int tari=0x7f040000;
     }
 }
